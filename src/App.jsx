8454946c3797162b0fffb04564676733366472ed
txxo <kisuat@hotmail.com>
2023-02-23 18:34:00 +0800

    t

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,23 @@ import styles from './App.module.css';
 
 function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+    <h1>Hello solid</h1>
+    // <div class={styles.App}>
+    //   <header class={styles.header}>
+    //     <img src={logo} class={styles.logo} alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.jsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       class={styles.link}
+    //       href="https://github.com/solidjs/solid"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn Solid
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
